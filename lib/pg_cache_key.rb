4c7a506fb7affe035ecdefac96e23fad641d226b
alekseyl <leshchuk@gmail.com>
2017-04-10 23:47:46 -0700

    remove obsolete comment

--- lib/pg_cache_key.rb
+++ lib/pg_cache_key.rb
@@ -1,20 +1,5 @@
 require "pg_cache_key/version"
 
-# In rails 5 it can be
-# module ActiveRecord
-#   module CollectionCacheKey
-#     def collection_cache_key(collection = all, timestamp_column = :updated_at) # :nodoc:
-#       # why use connection.execute instead of doing collection.select because of an order. if you using some order on your scope
-#       # then columns you using to order must appear in the GROUP BY clause or be used in an aggregate function or you will get an error
-#       cache_columns = [timestamp_column, :id]
-#       @cache_keys ||= {}
-#       # we need to add select cache_columns explicitly because if relation has includes it might transform columns to aliases
-#       @cache_keys[timestamp_column] ||= connection.execute( "SELECT md5(string_agg( #{cache_columns.map{|fld| "\"t\".\"#{fld}\"::text" }.join('||')}, '') ) as cache_key
-#                             FROM (#{ collection.select(cache_columns).try(:to_sql) }) t" )[0]['cache_key']
-#     end
-#   end
-# end
-
 module PgCacheKey
   def cache_key_raw_sql( timestamp_column = :updated_at )
     # Rem 1: why use connection.execute instead of doing collection.select because of an order. if you using some order on your scope
